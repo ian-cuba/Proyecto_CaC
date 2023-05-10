4a5ce59cf62ceab2e13dae85e61e57b949bffb39
Ian Luis Noa <noaianluis@gmail.com>
2023-05-09 22:22:46 -0300

    conexion contacto con mail

---
+++ js/api_contact.js
@@ -0,0 +1,24 @@
+let $form = document.querySelector('#contactForm')
+
+$form.addEventListener('submit', handleSubmit ) 
+
+async function handleSubmit (event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok) {
+        this.reset()
+        Swal.fire({
+            icon: 'success',
+            title: 'Gracias por Contactarnos!',
+            text: 'Nos comunicaremos con vos en la brevedad!',
+        })
+    }
+}
+
