97071c7c043180e6848703f811ebe866e5b90194
Angel <angel@MacBook-Pro-de-Angel.local>
2023-05-17 18:54:49 -0300

    implementacion de show-password en register

--- js/validate.js
+++ js/validate.js
@@ -82,6 +82,45 @@ function controlPassword() {
 
 
 // Validación de campos de los formularios de Register y Contact
+function confirmation(){
+    let password2 = document.getElementById(`password2`) ; 
+    let imgEye2 = document.getElementById(`eyeRegister2`) ;
+    let eye = "./img/login/ojo.png" ;
+    let notEye = "./img/login/ver.png"
+
+    if(password2.type===`password`){
+        password2.type=`text`
+        imgEye2.src=notEye
+    }else{
+        if(password2.type===`text`){
+            password2.type=`password`
+            imgEye2.src=eye
+        }
+    }
+
+}
+
+function passwordDisplay(){
+    let password = document.getElementById(`password`) ;
+    let imgEye = document.getElementById(`eyeRegister`) ;
+    
+    let eye = "./img/login/ojo.png" ;
+    let notEye = "./img/login/ver.png"
+
+    if(password.type===`password`){
+        password.type=`text`
+        imgEye.src=notEye
+    }else{
+        if(password.type===`text`){
+            password.type=`password`
+            imgEye.src=eye
+        }
+    }
+
+}
+
+
+
 function evalName() {
     let name = document.getElementById("name").value;
     let element = document.getElementById("nameStatus");
